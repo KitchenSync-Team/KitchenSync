e478d964da565f18e18bfc57f9b6f527e379f997
bswartz <bswartz@ohioschoolboards.org>
2025-10-27 17:41:07 -0400

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
           </main>
         </div>
-
+#comment
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
             Powered by{" "}
